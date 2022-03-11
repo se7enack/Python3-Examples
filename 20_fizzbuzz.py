9611e1ab7f3dd961cec3ab636363a19a51623c13
Stephen Burke <steburke71@hotmail.com>
2022-03-10 21:50:26 -0500

    .

--- 20_fizzbuzz.py
+++ 20_fizzbuzz.py
@@ -3,12 +3,13 @@
 fb=1
 
 while fb <= 100:
-    if 0 == fb % 3 + fb % 5:
-        print("fizzbuzz")
+    if 0 == fb % 3 and fb % 5:
+        print(fb, "fizzbuzz")
     elif 0 == fb % 3:
-        print("fizz")
+        print(fb, "fizz")
     elif 0 == fb % 5:
-        print("buzz")
+        print(fb, "buzz")
     else:
         print(fb)	
     fb = fb + 1 
+ 
